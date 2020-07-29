1f61714f93323d88276eabef73e7d772b87b2da8
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-29 16:23:55 -0400

    added const

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import jsonplaceholder from '../apis/jsonplaceholder';
 
-export const fetchPosts = () => {
+export const fetchPosts = async () => {
+   const respose = await jsonplaceholder.get('/posts');
+
   return {
     type: 'FETCH_POSTS'
   };
