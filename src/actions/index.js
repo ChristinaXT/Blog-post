5fdc7c9e9307ad96244b91009d639cc0835ce874
Christina Williams <christinaxtwilliams@gmail.com>
2020-07-29 17:28:59 -0400

    added fix

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,9 +4,9 @@ export const fetchPosts = () => {
   return function(dispatch, getState) {
    const promise = jsonPlaceholder.get('/posts');
 
-  return {
-    type: 'FETCH_POSTS',
-    payload: promise
-  };
- }
+    return {
+      type: 'FETCH_POSTS',
+      payload: promise
+    };
+  }
 };
