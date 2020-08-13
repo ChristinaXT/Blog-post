5c9ff0b1583e1e0c658df91aed17c92afc9f1833
Christina Williams <christinaxtwilliams@gmail.com>
2020-08-13 18:06:22 -0400

    map

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -6,10 +6,14 @@ class PostList extends React.Component {
   componentDidMount() {
     this.props.fetchPosts();
   }
-  
+
   render() {
     return <div> Post List</div>;
   }
+}
+
+const mapStateToProps = () => {
+  
 }
 
 export default connect(null, { fetchPosts })(PostList);
