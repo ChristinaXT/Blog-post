75776a2a007235b2869a5bc20319ff48cd094618
Christina Williams <christinaxtwilliams@gmail.com>
2020-08-17 16:17:04 -0400

    added style

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -11,8 +11,13 @@ class PostList extends React.Component {
      return this.props.posts.map(post => {
        return (
           <div className="item" key={post.id}>
-           < i className="large middle aligned icon user" />
-         
+           <i className="large middle aligned icon user" />
+           <div className="content">
+             <div className="description">
+               <h2>{post.title}</h2>
+               <p>{post.body}</p>
+            </div>
+           </div>
           </div>
        );
      });
