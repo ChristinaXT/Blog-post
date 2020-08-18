3d286aa9f8928404a6df4a845987029ded5a04b4
Christina Williams <christinaxtwilliams@gmail.com>
2020-08-18 15:56:41 -0400

    aded

--- src/actions/index.js
+++ src/actions/index.js
@@ -8,4 +8,6 @@ export const fetchPosts = () => async dispatch => {
 
   export const fecthUser = (id) => async dispatch => {
     const response = await jsonPlaceholder.get(`/users/${id}`);
+
+    dispatch({ type: 'FETCH_USER', payload: response.data });
   };
