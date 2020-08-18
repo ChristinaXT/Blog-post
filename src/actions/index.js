e2c38b3d8716ef0a30143ecd67fd547602f1ad1a
Christina Williams <christinaxtwilliams@gmail.com>
2020-08-18 15:51:49 -0400

    added action creater

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,3 +5,7 @@ export const fetchPosts = () => async dispatch => {
 
     dispatch({ type: 'FETCH_POSTS', payload: response.data })
   };
+
+  export const fecthUser = (id) => async dispatch => {
+    const response = await jsonPlaceholder.get() 
+  }
